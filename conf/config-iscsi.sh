6c9333a4c599f83289675a985baaec9297b7e291
muryliang <muryliang@gmail.com>
2017-05-05 09:11:58 +0800

    update

--- conf/config-iscsi.sh
+++ conf/config-iscsi.sh
@@ -19,8 +19,14 @@
 FILEIO=fileio_tgt
 TARGPATH="/sys/kernel/scst_tgt/targets/iscsi"
 TARGPFX="iqn.2016-01.com.lwstore"
-CFILE="/tmp/iscsiconf"
-TFILE="/tmp/.iscsiconf.in"
+CFILE="/etc/swgfs/iscsiconf"
+TFILE="/etc/swgfs/.iscsiconf.in"
+ALLOWFILE="/etc/initiators.allow"
+TALLOWFILE="/etc/.initiators.allow.in"
+DENYFILE="/etc/initiators.deny"
+TDENYFILE="/etc/.initiators.deny.in"
+DESCFILE="/etc/swgfs/descconf"
+TDESCFILE="/etc/swgfs/.descconf"
 export ERRFILE="/tmp/.error"
 
 check_exec()
@@ -59,23 +65,101 @@ get_block_file_pos()
 	done
 }
 
+check_ip ()
+{
+	res=$(echo $1 | egrep  '^([0-9]{1,3}\.){3}[0-9]{1,3}(,([0-9]{1,3}\.){3}[0-9]{1,3})*$')
+	if [ "x$res" == "x" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+add_to_allow_file()
+{
+	echo "$2 $1" >> $ALLOWFILE
+	echo "$2 ALL" >> $DENYFILE
+}
+
+del_from_allow_file()
+{
+	cat $ALLOWFILE | while read line
+	do
+		if [ "$1" != "$(echo $line | gawk '{print $1}')" ]
+		then
+			echo "$line" >> $TALLOWFILE
+		fi
+	done 
+	cat $DENYFILE | while read line
+	do
+		if [ "$1" != "$(echo $line | gawk '{print $1}')" ]
+		then
+			echo "$line" >> $TDENYFILE
+		fi
+	done 
+	rm -rf $ALLOWFILE 2>&1
+	cp $TALLOWFILE $ALLOWFILE
+	rm -f $TALLOWFILE
+	touch $ALLOWFILE
+	rm -rf $DENYFILE 2>&1
+	cp $TDENYFILE $DENYFILE
+	rm -f $TDENYFILE
+	touch $DENYFILE
+}
+
+add_to_desc_file()
+{
+        echo "$1 $2" >> $DESCFILE
+}
+
+del_from_desc_file()
+{
+        cat $DESCFILE | while read line
+        do
+                if [ "$1" != "$(echo $line | gawk '{print $1}')" ]
+                then
+                        echo "$line" >> $TDESCFILE
+                fi
+        done
+        rm -rf $DESCFILE 2>&1
+        mv $TDESCFILE $DESCFILE
+}
+
 do_create_iscsi()
 {
 	targname="${TARGPFX}:$1"
 	targname_path="${TARGPFX}\:$1"
 	lunpath="${TARGPATH}/${targname}/luns/mgmt"
 	
-	truncate -s ${2}g  ${3}/Iscsiblk_${1}_${1}
-	if [ "x$4" == "xwb" ]; then
-		nohup $FILEIO  $1 ${3}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
-	else
-		nohup $FILEIO -o $1 ${3}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
+	if [ "$1" != "$(echo $1 | egrep '^[a-zA-Z0-9]*$')" ]; then
+		echo "iscsi name only allow characters [a-zA-Z0-9]"
+		exit
 	fi
+	res=$(echo "${2}" | egrep '^[1-9][0-9]*$')
+	if [ "x$res" == "x" ]; then
+		echo "size invalid"
+		exit
+	fi
+	truncate -s ${2}g  ${3}/Iscsiblk_${1}_${1}
+	nohup $FILEIO  $1 ${3}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
 	echo "add_target $targname" > "${TARGPATH}/mgmt"
 	usleep 1000000
 	echo "add $1 0" > "$lunpath"
 	echo 1 > "${TARGPATH}/${targname}/enabled"
 	echo 1 > "${TARGPATH}/enabled"
+	if [ ! "x$4" == "x" ] ; then
+		ipaddr=$4
+		check_ip $ipaddr
+		res=$?
+		if [ "$res" != "0" ]; then
+			add_to_allow_file $ipaddr $targname
+			if [ "x$5" != "x" ];then #has description
+                                add_to_desc_file $1 "$5"
+                        fi
+                else
+                        add_to_desc_file $1 "$4"
+                fi
+	fi
 }
 
 do_recovery_iscsi()
@@ -88,11 +172,7 @@ do_recovery_iscsi()
 		echo "not exist, can not restore $1"
 		return 4
 	fi
-	if [ "x$3" == "xwb" ]; then
-		nohup $FILEIO  $1 ${2}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
-	else
-		nohup $FILEIO -o  $1 ${2}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
-	fi
+	nohup $FILEIO  $1 ${2}/Iscsiblk_${1}_${1} >> ${ISCSILOG}/${1} 2>&1 &
 	echo "add_target $targname" > "${TARGPATH}/mgmt"
 	usleep 1000000
 	echo "add $1 0" > "$lunpath"
@@ -116,6 +196,8 @@ do_delete_iscsi()
 	if [ "n$pid" != "n" ]; then
 		kill $pid > /dev/null 2>&1
 	fi
+	del_from_allow_file $targname
+	del_from_desc_file $1
 }
 
 do_unlink_iscsi()
@@ -156,7 +238,36 @@ do_remove_entry()
 
 do_read_config()
 {
-	cat $CFILE	2>&1
+	cat $CFILE | while read line
+        do
+		ipaddr=""	
+		tgname=$(echo $line | gawk '{print $1}')
+		listdir="${TARGPATH}/${TARGPFX}:${tgname}/sessions"
+		if [  -e $listdir ]; then
+			for tmpaddr in $(ls -R1  $listdir | egrep '^([0-9]{1,3}\.){3}[0-9]{1,3}$' | sort | uniq)
+			do
+				if [ "x$ipaddr" == "x" ]; then
+					ipaddr=${tmpaddr}
+				else
+					ipaddr="${tmpaddr},${ipaddr}"
+				fi
+			done
+		fi
+
+		name=${TARGPFX}:${tgname}
+		restrict=$(cat $ALLOWFILE | egrep "^$name " | cut -d ' ' -f 2)
+		desc=$(cat $DESCFILE | egrep "^$tgname " | cut -d ' ' -f 2- )
+		if [ "x$restrict" == "x" ]; then
+			restrict="default"
+		fi
+		if [ "x$ipaddr" == "x" ]; then
+			ipaddr="default"
+		fi
+		if [ "x$desc" == "x" ]; then
+			desc="default"
+		fi
+		echo $line $restrict $ipaddr $desc
+        done
 }
 
 if [ $# -lt 1 ]; then
@@ -171,14 +282,20 @@ ISCSILOG="/var/log/iscsi_log"
 mkdir -p "$ISCSILOG"
 [ ! -e $CFILE ] && touch $CFILE 
 [ ! -e $TFILE ] && touch $TFILE 
+[ ! -e $ALLOWFILE ] && touch $ALLOWFILE 
+[ ! -e $TALLOWFILE ] && touch $TALLOWFILE 
+[ ! -e $DENYFILE ] && touch $DENYFILE 
+[ ! -e $TDENYFILE ] && touch $TDENYFILE 
+[ ! -e $DESCFILE ] && touch $DESCFILE 
+[ ! -e $TDESCFILE ] && touch $TDESCFILE 
 touch $ERRFILE
 case $ACTION in 
 "load" )
 	load_mod
 	;;
 "create" )
-	if [ "$#" != "5" ]; then
-		echo "not enough param  should 3"
+	if [ "$#" != "5" -a "$#" != "6" -a "$#" != 7 ]; then
+		echo "not enough param  should be 5 or 6 or 7 params"
 		exit 1
 	fi
 	echo "right" > $ERRFILE
@@ -195,18 +312,17 @@ case $ACTION in
 	if [ "$error" == "exist" ];then
 		exit
 	fi
-	do_create_iscsi $2 $3 $5 $4
-	echo "$2 $4 $3 $5" >> "$CFILE"
+	do_create_iscsi $2 $3 $5 "$6" "$7"
+	echo "$2 wb $3 $5" >> "$CFILE"
 	;;
 "recovery" )
 	cat $CFILE | while read recv_line
 	do
 		name=$(echo $recv_line | gawk '{print $1}')
-		mode=$(echo $recv_line | gawk '{print $2}')
 		FILEDIR=$(get_block_file_pos $name)
 		exist=$(ps aux | grep fileio_tgt | grep ${name}_${name} | grep -v grep)
 		if [ -z "$exist" ]; then
-			do_recovery_iscsi $name  $FILEDIR $mode
+			do_recovery_iscsi $name  $FILEDIR
 		fi
 	done
 	;;
