17dd1722eb9384b956ba3f5bfe74b38a7aaefcfe
muryliang <muryliang@gmail.com>
2017-12-26 14:14:22 +0800

    update

--- about-programming/c++/jisuanke/practise/main.cc
+++ about-programming/c++/jisuanke/practise/main.cc
@@ -1,43 +1,129 @@
 #include <iostream>
-#include <functional>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
-auto Pair = [](auto u, auto v) {
-	return [=](auto f) {
-		return f(u, v);
-	};
+class Element {
+private:
+    int number;
+public:
+	Element() :number(0) {
+	    cout << "ctor" << endl;
+	}
+  	Element(int num):number(num) {
+  		cout << "ctor" << endl;
+  	}
+  	Element(const Element& e):number(e.number) {
+  		cout << "copy ctor" << endl;
+  	}
+  	Element(Element&& e):number(e.number) {
+  		cout << "right value ctor" << endl;
+  	}
+  	~Element() {
+  		cout << "dtor" << endl;
+  	}
+  	void operator=(const Element& item) {
+  		number = item.number;
+  	}
+  	bool operator==(const Element& item) {
+  		return (number == item.number);
+  	}
+  	void operator()() {
+  		cout << number ;
+  	}
+  	int GetNumber() {
+  		return number;
+  	}
 };
-auto First = [](auto t) {
-    return t([=](auto a, auto b) { 
-        return a;
-    });
-}; //TODO
-
-
-auto Second = [](auto t) {
-    return t([=](auto a, auto b) { 
-        return b;
-    });
-}; //TODO
-
-
-void PrintAll(nullptr_t) {
-
-}
 template<typename T>
-void PrintAll(T t) {
-	cout << First(t) << endl;
-	PrintAll(Second(t));
+class Vector {
+private:
+  	T* items;
+  	int count;
+public:
+  	Vector() :count{ 0 }, items{nullptr} {
+
+  	}
+  	Vector(const Vector& vector) :count{vector.count} {
+  		items = static_cast<T*>(malloc(sizeof(T) * count));
+  		memcpy(items, vector.items, sizeof(T) * count);
+  	}
+  	Vector(Vector&& vector) :count{ vector.count }, items{ vector.items } {
+  		//TODO
+        vector.items = nullptr;
+        vector.count = 0;
+  	}
+  	~Vector() {
+  		//TODO
+        free(items);
+        count = 0;
+  	}
+    T& operator[](int index){
+    	if (index<0||index>=count) {
+    		cout<<"invalid index"<<endl;
+    		return items[0];
+    	}
+    	return items[index];
+    }
+    int returnCount(){
+    	return count;
+    }
+  	void Clear() {
+  		//TODO
+        free(items);
+        count = 0;
+        items = nullptr;
+  	}
+
+  	void Add(const T& item) {
+  		//TODO
+
+  	}
+  	bool Insert(const T& item,int index) {
+  		//TODO
+  	}
+  	bool Remove(int index) {
+  		//TODO
+  	}
+  	int Contains(const T& item) {
+  		//TODO
+  	}
+};
+template<typename T>
+void PrintVector(Vector<T>& v){
+	  int count=v.returnCount();
+	  for (int i = 0; i < count; i++)
+	  {
+		  v[i]();
+		  cout << " ";
+	  }
+	  cout << endl;
 }
-
-int main()
-{
-	auto t = Pair(1, "two");
-	auto one = First(t);
-	auto two = Second(t);
-	cout << one << ", " << two << endl;
-	auto numbers = Pair(1, Pair(2, Pair(3, Pair(4, Pair(5, nullptr)))));
-	PrintAll(numbers);
-
-    return 0;
+int main() {
+  	Vector<Element>v;
+  	for (int i = 0; i < 4; i++) {
+  		Element e(i);
+  		v.Add(e);
+  	}
+  	PrintVector(v);
+  	Element e2(4);
+  	if (!v.Insert(e2, 10))
+  	{
+  		v.Insert(e2, 2);
+  	}
+  	PrintVector(v);
+  	if (!v.Remove(10))
+  	{
+  		v.Remove(2);
+  	}
+  	PrintVector(v);
+  	Element e3(1), e4(10);
+  	cout << v.Contains(e3) << endl;
+  	cout << v.Contains(e4) << endl;
+  	Vector<Element>v2(v);
+  	Vector<Element>v3(move(v2));
+  	PrintVector(v3);
+  	v2.Add(e3);
+  	PrintVector(v2);
+  	return 0;
 }
 
