593177a862d45936c81c42112f3fa7231ee8a072
muryliang <muryliang@gmail.com>
2018-11-02 18:03:04 +0800

    udpate

---
+++ qt_projects/justtest/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc !=4 ) {
+        qDebug() << "need gap sleep_time height";
+        return -1;
+    }
+    QApplication a(argc, argv);
+    MainWindow w(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
+    w.show();
+
+    return a.exec();
+}
---
+++ qt_projects/justtest/worker.h
@@ -0,0 +1,49 @@
+﻿#ifndef WORKER_H
+#define WORKER_H
+
+#include <QQueue>
+#include <QImage>
+#include <QByteArray>
+#include <QDebug>
+
+
+class Worker : public QObject {
+    Q_OBJECT
+public:
+    Worker(int usleep = 0, int pheight = 0);
+
+public slots:
+    void run();
+
+signals:
+    void sendImg(QImage);
+    void sendImgOne(QImage);
+    void sendImgData(QByteArray data, int width, int height);
+    void startloop();
+
+private:
+#ifdef __linux__
+    void runarm();
+#elif _WIN32
+    void runpc();
+#endif
+    inline bool blkscr(const char* data, int len) {
+        for(int i = 0; i < len; i+=2) {
+            if (data[i] > 0x0b) {
+                qDebug() << "not black screen, start";
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+private:
+    QVector<QByteArray>  qb;
+    QByteArray barr;
+    int _usleep;
+    int _pheight;
+};
+
+#endif // WORKER_H
