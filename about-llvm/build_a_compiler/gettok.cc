19be65b5dd6dcb683b29438ea9b0469f4e51e918
muryliang <muryliang@gmail.com>
2019-01-09 17:55:36 +0800

    udpate

--- about-llvm/build_a_compiler/gettok.cc
+++ about-llvm/build_a_compiler/gettok.cc
@@ -219,14 +219,6 @@ static int GetTokPrecedence() {
 	return TokPrec;
 }
 
-int main() {
-	BinopPrecedence['<'] = 10;
-	BinopPrecedence['+'] = 20;
-	BinopPrecedence['-'] = 20;
-	BinopPrecedence['*'] = 40;
-	return 0;
-}
-
 static std::unique_ptr<ExprAST> ParseExpression() {
 	auto LHS = ParsePrimary();
 	if (!LHS)
@@ -308,6 +300,35 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
 	return nullptr;
 }
 
+
+static void HandleDefinition() {
+  if (ParseDefinition()) {
+    fprintf(stderr, "Parsed a function definition.\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleExtern() {
+  if (ParseExtern()) {
+    fprintf(stderr, "Parsed an extern\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleTopLevelExpression() {
+  // Evaluate a top-level expression into an anonymous function.
+  if (ParseTopLevelExpr()) {
+    fprintf(stderr, "Parsed a top-level expr\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
 static void MainLoop() {
 	while (1) {
 		fprintf(stderr, "ready> ");
@@ -328,4 +349,22 @@ static void MainLoop() {
 				break;
 		}
 	}
+}
+
+int main() {
+  // Install standard binary operators.
+  // 1 is lowest precedence.
+  BinopPrecedence['<'] = 10;
+  BinopPrecedence['+'] = 20;
+  BinopPrecedence['-'] = 20;
+  BinopPrecedence['*'] = 40; // highest.
+
+  // Prime the first token.
+  fprintf(stderr, "ready> ");
+  getNextToken();
+
+  // Run the main "interpreter loop" now.
+  MainLoop();
+
+  return 0;
 }
